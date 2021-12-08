b2e6f8167525c7b4aa59784aa2cb2eb8cb3dd2ea
聂智豪 <niezhihao@meiqia.com>
2021-12-09 00:41:43 +0800

    update: 权限模块开发 - 添加权限模块层级对象转换类 - 解决引入 MapStruct 后与 Lomkbok 的冲突问题

---
+++ src/main/java/com/chuxin/eshop/auth/mapstruct/PrioritySwitcher.java
@@ -0,0 +1,49 @@
+package com.chuxin.eshop.auth.mapstruct;
+
+import com.chuxin.eshop.auth.domain.PriorityDO;
+import com.chuxin.eshop.auth.domain.PriorityDTO;
+import com.chuxin.eshop.auth.domain.PriorityVO;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+/**
+ * @author chuxin
+ * @ClassName PrioritySwitcher
+ * @Date 2021/12/9 12:12 AM
+ * @Description: 权限模块-层对象转换类
+ */
+@Mapper
+public interface PrioritySwitcher {
+
+    PrioritySwitcher INSTANCE = Mappers.getMapper(PrioritySwitcher.class);
+
+    /**
+     * 将 PriorityDTO 对象转换为 PriorityVO 对象
+     * @param priorityDTO
+     * @return PriorityVO 对象
+     */
+    PriorityVO dto2VO(PriorityDTO priorityDTO);
+
+    /**
+     * 将 PriorityDO 对象转换为 PriorityDTO 对象
+     * @param priorityDO
+     * @return PriorityDTO 对象
+     */
+    PriorityDTO do2DTO(PriorityDO priorityDO);
+
+    /**
+     * 将 PriorityDTO 的 List 转换成为 PriorityVO List
+     * @param dtoList PriorityDTO 对象集合
+     * @return PriorityVO 对象集合
+     */
+    List<PriorityVO> dtoList2VOList(List<PriorityDTO> dtoList);
+
+    /**
+     * 将 PriorityDO 的 List 转换成为 PriorityDTO List
+     * @param doList PriorityDO 对象集合
+     * @return PriorityDTO 对象集合
+     */
+    List<PriorityDTO> doList2DTOList(List<PriorityDO> doList);
+}
---
+++ src/main/java/com/chuxin/eshop/auth/mapstruct/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * @ClassName package-info
+ * @author chuxin
+ * @Date 2021/12/9 12:11 AM
+ * @Description: TODO
+ */
+package com.chuxin.eshop.auth.mapstruct;
